025b1544880af444082eae57c7966fb67277927d
Akai9866 <b16036@students.iitmandi.ac.in>
2017-11-12 19:22:11 +0530

    Add files via upload

--- AllProductsDisplay.php
+++ AllProductsDisplay.php
@@ -1,24 +1,34 @@
 <?php
-	include('config.php');
+	include('session.php');
+	if(!isset($_SESSION['shop_id'])) $_SESSION['shop_id'] = $_POST['secret_field'];
 	if(!isset($_SESSION['shop_id']))
 	{
 		echo "Shop id not found!";
 	}
 	else
 	{
-		$table_name = "Table" + (string)$_SESSION['shop_id'];
-		$sql = "SELECT * FROM '$table_name' WHERE 1";
+		$table_name = "tabl";
+		$table_name .= (string)$_SESSION['shop_id'];
+		$sql = "SELECT * FROM $table_name WHERE 1";
 		$result = mysqli_query($db, $sql);
-		
-		echo "<table>";
-		while($row = mysql_fetch_array($result))
+		if($result == false) 
 		{
-			$sql2 = "SELECT Name FROM Product_Table WHERE product_id = ".$row['product_id'];
-			$result2 = mysqli_query($db, $sql2);
-			$row2 = mysql_fetch_array($result2);
-			$pn = $row2['Name'];
-			echo "<tr><td>".$pn."</td><tf>".$row['Qty']."</td><td>".$row['price']."</td></tr>";
+			echo "No rows found<br/>";
 		}
-		echo "</table";
+		else
+		{
+			echo "<table>";
+			while($row = mysqli_fetch_array($result))
+			{
+				$sql2 = "SELECT product_name FROM Product_Table WHERE product_id = ".$row['product_id'];
+				$result2 = mysqli_query($db, $sql2);
+				$row2 = mysqli_fetch_array($result2);
+				$pn = $row2['product_name'];
+				echo "<tr><td>".$pn."</td><td>".$row['qty']."</td></tr>";
+			}
+			echo "</table>";
+		}
 	}
+	//echo $_SESSION['shop_id'];
 ?>
+<p><a href = "AddItems0.php">Add Items</a></p>
